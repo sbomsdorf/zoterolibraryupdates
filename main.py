7794a7cad40639cb0786cdc393a25d535946403e
Stefan Bomsdorf <bomsdorf@dpo.rwth-aachen.de>
2022-05-20 16:18:04 +0200

    Small changes.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 """
 First version of the Zotero Library Updates Mailer
 """
-
+import datetime
 import smtplib, ssl
 import requests
+import json
+from datetime import *
 
 # TODO: get details defined in config.yaml
 
@@ -39,17 +41,16 @@ def call_api():
 
     with requests.Session() as s:
         s.headers = {'Authorization': "Bearer " + api_key}
+        request_string = "https://api.zotero.org" + prefix + "/items/sort=dateAdded/direction=desc"
 
-        request_string = "https://api.zotero.org" + prefix + "/searches/"
-        searches = s.get(request_string)
-
-        for item in searches.json():
-            if item['data']['name'] == get_api_config()["saved_search_name"]:
-                search_key = item['data']['key']
-
-        request_string = "https://api.zotero.org" + prefix + "/searches?searchKey=" + search_key + "/items?format=bib"
         search = s.get(request_string)
         print(search)
+        sorted_json = search.json()
+        with open('json_data.json', 'w') as outfile:
+            outfile.write(json.dumps(sorted_json))
+
+        get_recent_entries_as_string(search.json(), get_api_config()["period_in_days"])
+
 
 def get_group_id(user_id, api_key):
     with requests.Session() as s:
@@ -57,6 +58,47 @@ def get_group_id(user_id, api_key):
         group_id = s.get("https://api.zotero.org" + "/users/" + user_id + "/groups?id").json()[0]['id']
         return str(group_id)
 
-call_api()
+
+def get_search_key(userprefix, api_key):
+    with requests.Session() as s:
+        s.headers = {'Authorization': "Bearer " + api_key}
+
+        request_string = "https://api.zotero.org" + userprefix + "/searches/"
+
+        for item in s.get(request_string).json():
+            if item['data']['name'] == get_api_config()["saved_search_name"]:
+                search_key = item['data']['key']
 
 
+def sort_json_by_date_modified(json_string):
+    return sorted(json_string, key=lambda x: datetime.strptime(x['data']['dateModified'][0:10], '%Y-%m-%d'))
+
+
+def get_recent_entries_as_string(items_json, period_in_days):
+    counter_true = 0
+    counter_false = 0
+
+    for item in items_json:
+        if datetime.today() - datetime.strptime(item['data']['dateModified'][0:10], '%Y-%m-%d') <= timedelta(days=period_in_days):
+            counter_true += 1
+            print(counter_true)
+            print(counter_false)
+            print(datetime.strptime(item['data']['dateAdded'][0:10], '%Y-%m-%d'))
+        else:
+            counter_false += 1
+            print(datetime.strptime(item['data']['dateAdded'][0:10], '%Y-%m-%d'))
+
+
+    print("------")
+    print("Included: " + str(counter_true))
+    print("Excluded: " + str(counter_false))
+
+
+def get_library_version(user_id, api_key):
+    with requests.Session() as s:
+        s.headers = {'Authorization': "Bearer " + api_key}
+        version = s.get("https://api.zotero.org" + "/users/" + get_group_id(user_id, api_key) + "/items?format=versions").json()
+        print(str(version))
+
+#call_api()
+get_library_version("8267074", "AwmIsxfG9L39daG3z8tSVcfr")
